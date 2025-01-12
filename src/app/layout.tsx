780f9d07938aae35d2ca6a822c78c1c2c3714b6c
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2025-01-12 07:04:22 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function ProtectedLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
